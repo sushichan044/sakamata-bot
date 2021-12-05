6e6b0e4bf673788dacdfe7f637ecb49992eb3aed
sushi-chaaaan <sushi_code@outlook.jp>
2021-12-05 16:52:38 +0900

    essential

---
+++ discordbot.py
@@ -0,0 +1,27 @@
+import discord
+import os
+import traceback
+from datetime import datetime, timedelta
+import sys
+import requests
+
+### イベントハンドラ一覧 #################################################
+# async def の後を変えるだけで実行されるイベンドが変わる
+# メッセージ受信時に実行：   on_message(message)
+# Bot起動時に実行：      on_ready(message)
+# リアクション追加時に実行:  on_reaction_add(reaction, user)
+# 新規メンバー参加時に実行： on_member_join(member)
+# ボイスチャンネル出入に実行： on_voice_state_update(member, before, after)
+###################################################################
+
+token = os.environ['DISCORD_BOT_TOKEN']
+
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+
+#Bootmsg-console
+@client.event
+async def on_ready():
+    print('logged in as {0.user}'.format(client))
+
+client.run(token)
