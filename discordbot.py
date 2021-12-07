e3c1d10a1d28f71d8e7153722fee8386e5091a8e
sushi-chaaaan <sushi_code@outlook.jp>
2021-12-07 23:21:43 +0900

    a

--- discordbot.py
+++ discordbot.py
@@ -88,7 +88,7 @@ async def test(ctx):
 async def user(ctx,id: int):
     user = bot.get_user(id)
     userregdate = user.created_at
-    bot.get_guild(guildid)
+    guild = bot.get_guild(guildid)
     member = guild.get_member(id)
     channel = bot.get_channel(logchannel)
     await channel.send(f'ユーザー名:{member},アカウント作成日時:{userregdate:%Y/%m/%d %H:%M:%S}')
