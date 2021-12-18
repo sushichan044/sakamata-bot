67067199345f6a83623b0ce716924e9cafdebd8b
sushi-chaaaan <sushi_code@outlook.jp>
2021-12-19 02:43:10 +0900

    a

--- discordbot.py
+++ discordbot.py
@@ -30,7 +30,7 @@ intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='/',intents=intents)
 
 
-
+'''
 #本番鯖IDなど
 
 guildid = 915910043461890078
@@ -46,7 +46,7 @@ logchannel = 916971090042060830
 vclogchannel = 916988601902989373
 commandchannel = 917788514903539794
 dmboxchannel = 918101377958436954
-'''
+
 
 #Bootmsg-serverlogchannel/console
 async def greet():
