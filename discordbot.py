e9960e56a6d2819bc039b7f080644270a80bf95f
sushi-chaaaan <sushi_code@outlook.jp>
2021-12-25 13:53:03 +0900

    aaa

--- discordbot.py
+++ discordbot.py
@@ -172,7 +172,6 @@ async def ping(ctx):
 async def sendlog(ctx,msg,descurl):
     channel = bot.get_channel(logchannel)
     now = datetime.utcnow()
-    selfuser = bot.get_user(bot.user)
     embed = discord.Embed(
     color = 3447003,
     description = msg,
@@ -181,7 +180,7 @@ async def sendlog(ctx,msg,descurl):
     )
     embed.set_author(
     name='実行ログ',
-    icon_url=selfuser.avatar_url,
+#    icon_url=selfuser.avatar_url,
     )
     embed.add_field(
         name='実行者',
