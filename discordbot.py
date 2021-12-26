f6cd28302820bc3a68cd5bee5ee92f63a90827e8
sushi-chaaaan <sushi_code@outlook.jp>
2021-12-26 17:30:19 +0900

    test2

--- discordbot.py
+++ discordbot.py
@@ -105,7 +105,7 @@ async def on_message_dispand(message):
         return
     elif message.content.startswith('/send-message','/edit-message','/send-dm'):
         return
-    elif message.content/endswith('中止に必要な承認人数: 1'):
+    elif message.content.endswith('中止に必要な承認人数: 1'):
         return
     else:
         await dispand(message)
