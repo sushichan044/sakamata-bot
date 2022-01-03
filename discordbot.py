cbcda183ec19ad828fafde5956db9e3e0c15bfcb
sushi-chaaaan <sushi_code@outlook.jp>
2022-01-03 15:59:40 +0900

    a

--- discordbot.py
+++ discordbot.py
@@ -323,7 +323,7 @@ async def user(ctx,id:int):
     membermention = member.mention
     roles = [[x.name,x.id] for x in member.roles]
     print(roles)#[[name,id],[name,id]...]
-    zroles = [['/ID:'.join(x)]for x in roles] #[name/ID:id,...]
+    zroles = ['/ID:'.join([str(x) for x in roles])] #[name/ID:id,...]
     print(zroles)
     jlrole = '\n'.join(zroles)
     #Message成形-途中
