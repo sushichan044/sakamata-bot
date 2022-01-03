1a461cdac0e57f7c453a15426b7648423228c7c6
sushi-chaaaan <sushi_code@outlook.jp>
2022-01-03 15:36:50 +0900

    a

--- discordbot.py
+++ discordbot.py
@@ -322,7 +322,7 @@ async def user(ctx,id:int):
     memberjoindate = member.joined_at + timedelta(hours=9)
     membermention = member.mention
     roles = [[x.name,x.id] for x in member.roles]
-    zroles = [[',ID:'.join(str(x))]for x in roles]
+    zroles = [[',ID:'.join(str(x)for x in roles)]]
     jlrole = '\n'.join(zroles)
     #Message成形-途中
     userinfomsg = f'```ユーザー名:{member} (ID:{memberid})\nBot?:{memberifbot}\nニックネーム:{memberifnickname}\nアカウント作成日時:{memberregdate:%Y/%m/%d %H:%M:%S}\n参加日時:{memberjoindate:%Y/%m/%d %H:%M:%S}\n所持ロール:{jlrole}```'
