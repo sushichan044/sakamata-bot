f94314dea96944e457f5c87b6492b2aa300dc315
sushi-chaaaan <sushi_code@outlook.jp>
2022-01-06 17:28:06 +0900

    fix user

--- discordbot.py
+++ discordbot.py
@@ -401,7 +401,7 @@ async def user(ctx,id:int):
 @permissions.has_role(modrole)
 async def _newuser(
     ctx,
-    id: Option(str, '対象のIDを入力してください。'),
+    id: Option(Member, '対象のIDや名前を入力してください。'),
 ):
     '''ユーザー情報を取得できます。'''
     guild = ctx.guild
