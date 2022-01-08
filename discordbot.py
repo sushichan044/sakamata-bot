1d9aacdc8d1326ecc51610f2d57315a4c952e383
sushi-chaaaan <sushi_code@outlook.jp>
2022-01-09 00:16:20 +0900

    update member

--- discordbot.py
+++ discordbot.py
@@ -991,8 +991,8 @@ async def _check_member(ctx):
                 return
             else:
                 msg = non_exe_msg
-                desc_url = ''
-                await channel.send('DMで送信する不承認理由を入力してください。')
+                desc_url = tracker.message.jump_url
+                await tracker.message.reply(content='DMで送信する不承認理由を入力してください。', mention_author=False)
 
                 def check(message):
                     return message.content is not None and message.channel == channel and message.author != bot.user
