7bcfefd9aec206b3b9b702cf58cf658bbf9624c0
sushi-chaaaan <sushi_code@outlook.jp>
2022-01-10 16:57:57 +0900

    fix timestamp

--- discordbot.py
+++ discordbot.py
@@ -1031,7 +1031,7 @@ async def get_stream_method(ctx):
             live_title = x.title
             fixed_start_scheduled = x.start_scheduled.replace('Z', '+00:00')
             live_start = datetime.fromisoformat(fixed_start_scheduled).astimezone(jst)
-            live_start_timestamp = live_start.timestamp()
+            live_start_timestamp = int(live_start.timestamp())
             live_start_str = datetime.strftime(live_start, '%Y/%m/%d %H:%M')
             embed = discord.Embed(
                 title=f'{live_title}',
