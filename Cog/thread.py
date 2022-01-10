5f3e5742e4745f3e3736a778d613540a175b7b69
sushi-chaaaan <sushi_code@outlook.jp>
2022-01-10 10:05:37 +0900

    fuck auto import

--- Cog/thread.py
+++ Cog/thread.py
@@ -2,7 +2,7 @@ import os
 from datetime import timedelta, timezone
 
 import discord
-from discord import commands
+from discord.ext import commands
 
 thread_log_channel = os.environ['THREAD_LOG_CHANNEL']
 jst = timezone(timedelta(hours=9), 'Asia/Tokyo')
