1e2bad0ca15309bd93c620c6f7e88b6590e81705
sushi-chaaaan <sushi_code@outlook.jp>
2022-01-10 19:06:44 +0900

    fixed db connect

--- connect.py
+++ connect.py
@@ -5,6 +5,5 @@ import os
 def connect():
     return redis.from_url(
         url=os.environ.get('REDIS_URL'),  # 環境変数にあるURLを渡す
-        charset='utf-8',
         decode_responses=True,  # 日本語の文字化け対策のため必須
     )
