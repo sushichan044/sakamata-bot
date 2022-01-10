c1b49404837609f392f22c51f0ff434374aa699d
sushi-chaaaan <sushi_code@outlook.jp>
2022-01-10 09:11:35 +0900

    cog-test-poll

---
+++ Cog/poll.py
@@ -0,0 +1,83 @@
+import os
+import discord
+from discord.ext import commands
+
+server_member_role = os.environ['SERVER_MEMBER_ROLE']
+
+
+# PollEmoji
+poll_emoji_list = [
+    '\N{Large Red Circle}',
+    '\N{Large Green Circle}',
+    '\N{Large Orange Circle}',
+    '\N{Large Blue Circle}',
+    # '\N{Large Yellow Circle}',
+    '\N{Large Brown Circle}',
+    '\N{Large Purple Circle}',
+    # '\N{Medium Black Circle}',
+    # '\N{Medium White Circle}',
+    '\N{Large Red Square}',
+    '\N{Large Green Square}',
+    '\N{Large Orange Square}',
+    '\N{Large Blue Square}',
+    # '\N{Large Yellow Square}',
+    '\N{Large Brown Square}',
+    '\N{Large Purple Square}',
+    # '\N{Black Large Square}',
+    # '\N{White Large Square}',
+    '\N{Large Orange Diamond}',
+    '\N{Large Blue Diamond}',
+    '\N{Heavy Black Heart}',
+    '\N{Green Heart}',
+    '\N{Orange Heart}',
+    '\N{Blue Heart}',
+    '\N{Brown Heart}',
+    '\N{Purple Heart}',
+]
+
+
+class PollCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+        @commands.command(name='poll')
+        @commands.has_role(server_member_role)
+        async def _poll(self, ctx, title, *select):
+            if select == ():
+                embed = discord.Embed(
+                    title=title,
+                    description="\N{Large Green Circle}Yes\n\N{Large Red Circle}No",
+                    color=3447003,
+                )
+                poll_yes_emoji = '\N{Large Green Circle}'
+                poll_no_emoji = '\N{Large Red Circle}'
+                m = await ctx.send(embed=embed)
+                await m.add_reaction(poll_yes_emoji)
+                await m.add_reaction(poll_no_emoji)
+                return
+            elif len(select) > 20:
+                embed = discord.Embed(
+                    title='選択肢が多すぎます。',
+                    color=16098851,
+                )
+                await ctx.send(embed=embed)
+                return
+            else:
+                send_desc_list = []
+                for num in range(len(select)):
+                    element = f'{poll_emoji_list[num]}{select[num]}'
+                    send_desc_list.append(element)
+                send_desc = '\n'.join(send_desc_list)
+                embed = discord.Embed(
+                    title=title,
+                    description=send_desc,
+                    color=3447003,
+                )
+                m = await ctx.send(embed=embed)
+                for x in range(len(select)):
+                    await m.add_reaction(poll_emoji_list[x])
+                return
+
+
+def setup(bot):
+    return bot.add_cog(PollCog(bot))
--- discordbot.py
+++ discordbot.py
@@ -66,7 +66,7 @@ member_check_channel = 926777825925677096
 thread_log_channel = 927110282675884082
 join_log_channel = 929015822272299038
 count_vc = 925256795491012668
-server_member_role = 915915792275632139
+server_member_role = os.environ['SERVER_MEMBER_ROLE']
 mod_role = 916726433445986334
 admin_role = 915954009343422494
 everyone = 915910043461890078
@@ -84,7 +84,7 @@ member_check_channel = 926777719964987412
 thread_log_channel = 927110073996693544
 join_log_channel = 929015770539761715
 count_vc = 925249967478673519
-server_member_role = 923719282360188990
+server_member_role = os.environ['SERVER_MEMBER_ROLE']
 mod_role = 924355349308383252
 admin_role = 917332284582031390
 everyone = 916965252896260117
@@ -643,75 +643,6 @@ async def _editmessage(ctx, channel_id: int, message_id: int, *, arg):
     else:
         return
 
-# PollEmoji
-poll_emoji_list = [
-    '\N{Large Red Circle}',
-    '\N{Large Green Circle}',
-    '\N{Large Orange Circle}',
-    '\N{Large Blue Circle}',
-    # '\N{Large Yellow Circle}',
-    '\N{Large Brown Circle}',
-    '\N{Large Purple Circle}',
-    # '\N{Medium Black Circle}',
-    # '\N{Medium White Circle}',
-    '\N{Large Red Square}',
-    '\N{Large Green Square}',
-    '\N{Large Orange Square}',
-    '\N{Large Blue Square}',
-    # '\N{Large Yellow Square}',
-    '\N{Large Brown Square}',
-    '\N{Large Purple Square}',
-    # '\N{Black Large Square}',
-    # '\N{White Large Square}',
-    '\N{Large Orange Diamond}',
-    '\N{Large Blue Diamond}',
-    '\N{Heavy Black Heart}',
-    '\N{Green Heart}',
-    '\N{Orange Heart}',
-    '\N{Blue Heart}',
-    '\N{Brown Heart}',
-    '\N{Purple Heart}',
-]
-# Poll
-
-
-@bot.command(name='poll')
-@commands.has_role(server_member_role)
-async def _poll(ctx, title, *select):
-    if select == ():
-        embed = discord.Embed(
-            title=title,
-            description="\N{Large Green Circle}Yes\n\N{Large Red Circle}No",
-            color=3447003,
-        )
-        poll_yes_emoji = '\N{Large Green Circle}'
-        poll_no_emoji = '\N{Large Red Circle}'
-        m = await ctx.send(embed=embed)
-        await m.add_reaction(poll_yes_emoji)
-        await m.add_reaction(poll_no_emoji)
-        return
-    elif len(select) > 20:
-        embed = discord.Embed(
-            title='選択肢が多すぎます。',
-            color=16098851,
-        )
-        await ctx.send(embed=embed)
-        return
-    else:
-        send_desc_list = []
-        for num in range(len(select)):
-            element = f'{poll_emoji_list[num]}{select[num]}'
-            send_desc_list.append(element)
-        send_desc = '\n'.join(send_desc_list)
-        embed = discord.Embed(
-            title=title,
-            description=send_desc,
-            color=3447003,
-        )
-        m = await ctx.send(embed=embed)
-        for x in range(len(select)):
-            await m.add_reaction(poll_emoji_list[x])
-        return
 
 # deal-member
 # deal:対処。ban/kick
@@ -1361,4 +1292,6 @@ async def _send_member_log(member, status):
 
 
 start_count.start()
+
+bot.load_extension("Cog.poll")
 bot.run(token)
