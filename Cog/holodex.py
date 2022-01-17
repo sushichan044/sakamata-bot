9403673152c2682287e12290c03c7c460748e712
sushi-chaaaan <sushi_code@outlook.jp>
2022-01-17 20:51:43 +0900

    wakaranai

--- Cog/holodex.py
+++ Cog/holodex.py
@@ -58,7 +58,7 @@ class StreamNotify(commands.Cog):
                     continue
                 else:
                     conn.set(f'{x.id}', 'notified', ex=604800)
-                    date, time, timestamp, weekday_str, created = TimeData().time_schedule(x)
+                    date, time, timestamp, weekday_str, created = TimeData.time_schedule(x)
                     embed = discord.Embed(
                         title=f'{x.title}',
                         description='**待機所が作成されました**',
@@ -96,7 +96,7 @@ class StreamNotify(commands.Cog):
                 result = conn.get(x.id)
                 if result == 'notified':
                     conn.set(f'{x.id}', 'started', ex=604800)
-                    actual_start = TimeData().time_going(x)
+                    actual_start = TimeData.time_schedule(x)
                     embed = discord.Embed(
                         title=f'{x.title}',
                         description='**ライブストリーミングが開始されました**',
@@ -122,7 +122,7 @@ class StreamNotify(commands.Cog):
                 result = conn.get(x.id)
                 if result == 'started':
                     conn.set(f'{x.id}', 'ended', ex=604800)
-                    actual_end, end_date, end_time, duration_str, weekday_str = TimeData().time_ended(x)
+                    actual_end, end_date, end_time, duration_str, weekday_str = TimeData.time_schedule(x)
                     embed = discord.Embed(
                         title=f'{x.title}',
                         description='**ライブストリーミングが終了しました**',
