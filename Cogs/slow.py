6d357af833613d2158f44047d0d9c1c8442db7f4
sushi-chaaaan <sushi_code@outlook.jp>
2022-01-20 19:32:37 +0900

    test

--- Cogs/slow.py
+++ Cogs/slow.py
@@ -16,16 +16,14 @@ class SlowMode(commands.Cog):
     @message_command(guild_ids=[guild_id], name='スローモード')
     @permissions.has_role(server_member_role)
     async def _slow_mode(self, ctx, message: discord.Message):
-        channel = self.bot.get_channel(message.channel)
-        await channel.edit(slowmode_delay=60)
+        await message.channel.edit(slowmode_delay=60)
         await ctx.respond('スローモードをONにしました。', ephemeral=True)
         return
 
     @message_command(guild_ids=[guild_id], name='スローモード解除')
     @permissions.has_role(server_member_role)
     async def _un_slow_mode(self, ctx, message: discord.Message):
-        channel = self.bot.get_channel(message.channel)
-        await channel.edit(slowmode_delay=0)
+        await message.channel.edit(slowmode_delay=0)
         await ctx.respond('スローモードをOFFにしました。', ephemeral=True)
         return
 
