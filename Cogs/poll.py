95834f5355de0d5e4964f356a3c983da1ce606ca
sushi-chaaaan <sushi_code@outlook.jp>
2022-01-21 01:32:01 +0900

    a

--- Cogs/poll.py
+++ Cogs/poll.py
@@ -94,7 +94,7 @@ class Poll(commands.Cog):
 
     @message_command(guild_ids=[guild_id], name='投票集計')
     async def _result_poll(self, ctx, message: discord.Message):
-        if not message.embeds or message.embeds[0].author.name != '投票':
+        if message.author != self.bot.user or not message.embeds or message.embeds[0].author.name != '投票':
             await ctx.respond('集計に対応していないメッセージです。', ephemeral=True)
             return
         else:
