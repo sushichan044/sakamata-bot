274e530bd8ed4afc81149c173beeeee9f1512a9f
sushi-chaaaan <sushi_code@outlook.jp>
2022-01-20 19:44:44 +0900

    test slow slash

--- Cogs/slow.py
+++ Cogs/slow.py
@@ -1,8 +1,12 @@
+from email.policy import default
 import os
+from typing import Literal
+
+from discord import Option
 
 import discord
 from discord import permissions
-from discord.commands import message_command
+from discord.commands import message_command, slash_command
 from discord.ext import commands
 
 guild_id = int(os.environ['GUILD_ID'])
@@ -26,6 +30,29 @@ class SlowMode(commands.Cog):
         await message.channel.edit(slowmode_delay=0)
         await ctx.respond('スローモードをOFFにしました。', ephemeral=True)
         return
+
+    @slash_command(guild_ids=[guild_id], name='slow')
+    @permissions.has_role(server_member_role)
+    async def _slash_slow(
+        self,
+        ctx,
+        channel: Option(discord.channel, 'Choose Channel'),
+        switch: Option(str, 'Choose ON/OFF', choices=['ON,OFF'], default='ON')
+    ):
+        if self.on_or_off(switch):
+            delay = 60,
+            deal = 'ON'
+        else:
+            delay = 0,
+            deal = 'OFF'
+        await channel.edit(slowmode_delay=delay)
+        await ctx.respond(f'スローモードを{deal}にしました。', ephemeral=True)
+
+    def on_or_off(self, switch: Literal['ON', 'OFF']) -> bool:
+        if switch == 'ON':
+            return True
+        else:
+            return False
 
 
 def setup(bot):
