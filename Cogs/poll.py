10818f41e153cd6031b25909bc480aed363c5de3
sushi-chaaaan <sushi_code@outlook.jp>
2022-01-20 22:45:05 +0900

    fix

--- Cogs/poll.py
+++ Cogs/poll.py
@@ -100,10 +100,10 @@ class Poll(commands.Cog):
         for value, count in d.items():
             embed.add_field(
                 name=value,
-                value=str(count-1)
+                value=f'{str(count-1)}票'
             )
         await message.reply(embed=embed)
-        await ctx.respond('集計完了', ephemeral=True)
+        await ctx.respond('集計完了')
         pass
 
 
