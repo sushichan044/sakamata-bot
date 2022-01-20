345ac6b8255315d2916fe5fd59b6f01bff634ff5
sushi-chaaaan <sushi_code@outlook.jp>
2022-01-20 14:15:00 +0900

    a

--- Cog/pin.py
+++ Cog/pin.py
@@ -17,17 +17,14 @@ class ContextPin(commands.Cog):
     @message_command(guild_ids=[guild_id], name='ピン留め')
     @permissions.has_role(server_member_role)
     async def _pin(self, ctx, message: discord.Message):
-        try:
-            await message.pin()
-        except HTTPException as e:
-            if e.code == 50021:
-                await ctx.respond('システムメッセージをピン留めすることはできません。', ephemeral=True)
-            elif e.code == 30003:
-                await ctx.respond('このチャンネルのピン留め数が上限に達しています。', ephemeral=True)
-            else:
-                return
+        if message.type != discord.MessageType.default:
+            await ctx.respond('システムメッセージをピン留めすることはできません！', ephemeral=True)
+            return
+        elif len(await message.channel.pins()) == 50:
+            await ctx.respond('このチャンネルのピン留め数が上限に達しています。', ephemeral=True)
+            return
         else:
-            await ctx.respond('ピン留めしました!', ephemeral=True)
+            await ctx.respond('ピン留めしました！', ephemeral=True)
             return
 
 
