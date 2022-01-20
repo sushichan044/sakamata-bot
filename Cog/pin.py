9d8d596f3af4b6259feaef1e8ee90f39e4ff8002
sushi-chaaaan <sushi_code@outlook.jp>
2022-01-20 13:19:34 +0900

    a

--- Cog/pin.py
+++ Cog/pin.py
@@ -21,8 +21,11 @@ class ContextPin(commands.Cog):
             await ctx.respond('ピン留めしました!', ephemeral=True)
             return
         except discord.HTTPException as e:
-            if e.code == 400:
+            if e.code == 50021:
                 await ctx.respond('システムメッセージをピン留めすることはできません。', ephemeral=True)
+                return
+            else:
+                await ctx.respond('予期せぬエラーが発生しました。', ephemeral=True)
                 return
 
 
