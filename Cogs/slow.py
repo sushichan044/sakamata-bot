e8f0957a7cb4fcf737d84d3286b502254ae47d3c
sushi-chaaaan <sushi_code@outlook.jp>
2022-01-20 20:24:19 +0900

    fix permission

--- Cogs/slow.py
+++ Cogs/slow.py
@@ -9,7 +9,7 @@ from discord.commands import message_command, slash_command
 from discord.ext import commands
 
 guild_id = int(os.environ['GUILD_ID'])
-server_member_role = int(os.environ['SERVER_MEMBER_ROLE'])
+mod_role = int(os.environ['MOD_ROLE'])
 
 
 class SlowMode(commands.Cog):
@@ -17,21 +17,21 @@ class SlowMode(commands.Cog):
         self.bot = bot
 
     @message_command(guild_ids=[guild_id], name='スローモード')
-    @permissions.has_role(server_member_role)
+    @permissions.has_role(mod_role)
     async def _slow_mode(self, ctx, message: discord.Message):
         await message.channel.edit(slowmode_delay=60)
         await ctx.respond('スローモードをONにしました。', ephemeral=True)
         return
 
     @message_command(guild_ids=[guild_id], name='スローモード解除')
-    @permissions.has_role(server_member_role)
+    @permissions.has_role(mod_role)
     async def _un_slow_mode(self, ctx, message: discord.Message):
         await message.channel.edit(slowmode_delay=0)
         await ctx.respond('スローモードをOFFにしました。', ephemeral=True)
         return
 
     @slash_command(guild_ids=[guild_id], name='slow')
-    @permissions.has_role(server_member_role)
+    @permissions.has_role(mod_role)
     async def _slash_slow(
         self,
         ctx,
