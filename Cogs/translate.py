ddce90e29695542b723b187d52a7568a334d386a
sushi-chaaaan <sushi_code@outlook.jp>
2022-01-21 21:51:04 +0900

    fix UI

--- Cogs/translate.py
+++ Cogs/translate.py
@@ -119,14 +119,9 @@ class Translate(commands.Cog):
         return result
 
     def compose_embed(self, origin: str, result: str, output: Literal['ja', 'en'], service: Literal['DeepL', 'GoogleTrans']):
-        if output == 'ja':
-            _footer = f'{service}によって翻訳されました'
-            _origin = '[翻訳元/Origin]'
-            _result = '[翻訳結果/Result]'
-        else:
-            _footer = f'Translated by {service}'
-            _origin = '[翻訳元/Origin]'
-            _result = '[翻訳結果/Result]'
+        _footer = f'Translated by {service}'
+        _origin = '[翻訳元/Origin]'
+        _result = '[翻訳結果/Result]'
         embeds = []
         origin_embed = discord.Embed(
             color=15767485,
