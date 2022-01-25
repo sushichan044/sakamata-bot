53215731bf770a62a9048eb0347203589ce56b59
sushi-chaaaan <sushi_code@outlook.jp>
2022-01-25 15:00:43 +0900

    ???????

--- Cogs/thread.py
+++ Cogs/thread.py
@@ -144,7 +144,7 @@ class EscapeButton(View):
 
     async def _ok(self, interaction: discord.Interaction):
         self.status = True
-        await interaction.message.edit(content=f'```\n{self.text}\n```')
+        await interaction.response.edit_message(content=f'```\n{self.text}\n```', embed=None, view=None)
         self.stop()
         return
 
