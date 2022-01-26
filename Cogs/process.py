ef2acee663d061edf5fd58e269998a1648a69ee5
sushichaaaan <sushi_code@outlook.jp>
2022-01-27 02:40:50 +0900

    test

--- Cogs/process.py
+++ Cogs/process.py
@@ -112,7 +112,10 @@ class JoinButton(View):
 
     async def _ok(self, interacton: discord.Interaction):
         conn.sadd(str(interacton.message.id), str(interacton.user.id))
-        await interacton.response.send_message('参加登録を行いました！\n開始までしばらくお待ちください！', ephemeral=True)
+        if not interacton.response.is_done():
+            await interacton.response.send_message('参加登録を行いました！\n開始までしばらくお待ちください！', ephemeral=True)
+        else:
+            print(interacton.followup)
         self.stop()
 
     async def body(self) -> Message:
