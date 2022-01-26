6b821906a31e62fc5a3924eca874eebe9eb24851
sushichaaaan <sushi_code@outlook.jp>
2022-01-27 01:46:01 +0900

    test join game

--- Cogs/process.py
+++ Cogs/process.py
@@ -1,14 +1,19 @@
 import asyncio
+from datetime import datetime, timedelta, timezone
 import os
 
 import discord
 from discord.commands import slash_command
 from discord.ext import commands
-from discord.ext.ui import (Button, InteractionProvider, Message, View,
+from discord.ext.ui import (Button, InteractionProvider, MessageProvider, Message, View,
                             ViewTracker, state)
+from Cogs.connect import connect
 
 guild_id = int(os.environ['GUILD_ID'])
 process_channel = int(os.environ['PROCESS_CATEGORY'])
+utc = timezone.utc
+jst = timezone(timedelta(hours=9), 'Asia/Tokyo')
+conn = connect()
 
 
 class Process(commands.Cog):
@@ -16,14 +21,28 @@ class Process(commands.Cog):
         self.bot = bot
 
     @slash_command(guild_ids=[guild_id], name='process')
-    async def _start_game(self, ctx):
-        view = JoinButton(ctx)
+    async def _start_game(self, ctx: discord.ApplicationContext) -> None:
+        view = CloseButton(ctx)
         tracker = ViewTracker(view, timeout=None)
         await tracker.track(InteractionProvider(ctx.interaction, ephemeral=True))
+        future = asyncio.Future()
+        await self._send_invite(ctx, future)
+        return
+
+    async def _send_invite(self, ctx: discord.ApplicationContext, future: asyncio.Future):
+        channel = ctx.interaction.channel
+        start_time = discord.utils.utcnow()
+        exp_time = start_time + timedelta(minutes=10.0)
+        view = JoinButton(ctx, start_time, exp_time)
+        tracker = ViewTracker(view, timeout=None)
+        await tracker.track(MessageProvider(channel))
+        await asyncio.sleep(30)
+        ids = conn.smembers(tracker.message.id)
+        print(ids)
         return
 
 
-class JoinButton(View):
+class CloseButton(View):
     status = state('status')
     l_str = state('l_str')
     r_str = state('r_str')
@@ -37,7 +56,7 @@ class JoinButton(View):
         self.r_str = '取り消し'
         self.status = None
         self.title = '募集を開始しました。'
-        self.text = '締め切る際はこのメッセージの締め切りボタンを押してください。\n募集を取り消す場合は取り消しボタンを押してください。'
+        self.text = '締め切る際は締め切りボタンを押してください。\n募集を取り消す場合は取り消しボタンを押してください。'
 
     async def _ok(self, interaction: discord.Interaction):
         self.status = True
@@ -69,20 +88,27 @@ class JoinButton(View):
         )
 
 
-class CloseButton(View):
+class JoinButton(View):
     status = state('status')
     text = state('text')
 
-    def __init__(self, ctx):
+    def __init__(self, ctx, start: datetime, exp: datetime):
         super().__init__()
         self.ctx = ctx
         self.text = '参加'
         self.status = None
+        self.start = start
+        self.exp = exp
+
+    async def _ok(self, interacton: discord.Interaction):
+        conn.sadd(str(interacton.message.id), str(interacton.user.id))
+        self.stop()
 
     async def body(self) -> Message:
+        exp_str = self.exp.astimezone(jst).strftime('%Y/%m/%d %H:%M:%S')
         embed = discord.Embed(
-            title='募集を開始しました。',
-            description='この内容で更新用メッセージを送信しますか？',
+            title='募集が開始されました。',
+            description=f'有効期限:{exp_str}',
             color=15767485,
         )
         embed.set_author(
@@ -92,7 +118,7 @@ class CloseButton(View):
         return Message(
             embeds=[embed],
             components=[
-                Button(self.str).style(discord.ButtonStyle.green).disabled(
+                Button(self.text).style(discord.ButtonStyle.green).disabled(
                     self.status is not None).on_click(self._ok),
             ]
         )
