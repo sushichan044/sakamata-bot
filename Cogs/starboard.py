148f6fbb6fe22cc63ef2b7235908acfebb5d9ba8
sushichaaaan <sushi_code@outlook.jp>
2022-01-29 08:17:47 +0900

    change file strcture

--- Cogs/starboard.py
+++ Cogs/starboard.py
@@ -5,8 +5,6 @@ import discord
 from discord.errors import HTTPException
 from discord.ext import commands
 
-from ..Data.starboard_data import _return_exception
-
 # star_emoji = '\N{Blue Heart}'
 star_emoji = '<:c_Ofb4:926885084395606086>'
 emoji_url = 'https://cdn.discordapp.com/emojis/926885084395606086.webp?size=1024&quality=lossless'
@@ -143,6 +141,32 @@ class StarBoard(commands.Cog):
             return True
         else:
             return False
+
+
+def _return_exception(env: Literal['main', 'alpha']) -> tuple[list[int], list[int]]:
+    ignore_category = {
+        916898415349223484: 'main',
+        927568480985821185: 'main',
+        925236277262045225: 'main',
+        917599766353969173: 'main',
+        916899483391000577: 'main',
+        926866496137879592: 'main',
+        917009372129919006: 'main',
+        915910929240191006: 'main',
+        917568001300123678: 'main',
+        916859441821929472: 'main',
+        926268191037087755: 'alpha',
+    }
+
+    ignore_channel = {
+        929345604805611520: 'main',
+        923789832482873404: 'main',
+        927555978092748800: 'main',
+        936758638561857537: 'alpha'
+    }
+    category = [cat for cat, e in ignore_category.items() if e == env]
+    channel = [ch for ch, e in ignore_channel.items() if e == env]
+    return category, channel
 
 
 def setup(bot):
