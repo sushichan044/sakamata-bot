8cbed42bf3c639b69a591c34e0a555e1095412c7
sushichaaaan <sushi_code@outlook.jp>
2022-02-10 02:33:48 +0900

    format vars

--- discordbot.py
+++ discordbot.py
@@ -140,30 +140,13 @@ guild_id = int(os.environ["GUILD_ID"])
 
 # ID-role
 everyone = int(os.environ["GUILD_ID"])
-server_member_role = int(os.environ["SERVER_MEMBER_ROLE"])
 mod_role = int(os.environ["MOD_ROLE"])
-admin_role = int(os.environ["ADMIN_ROLE"])
-yt_membership_role = int(os.environ["YT_MEMBER_ROLE"])
 stop_role = int(os.environ["STOP_ROLE"])
 vc_stop_role = int(os.environ["VC_STOP_ROLE"])
 
-# ID-channel
-alert_channel = int(os.environ["ALERT_CHANNEL"])
-stream_channel = int(os.environ["STREAM_CHANNEL"])
-star_channel = int(os.environ["STAR_CHANNEL"])
-dm_box_channel = int(os.environ["DM_BOX_CHANNEL"])
-alert_channel = int(os.environ["ALERT_CHANNEL"])
-member_check_channel = int(os.environ["MEMBER_CHECK_CHANNEL"])
 
 # ID-log
-thread_log_channel = int(os.environ["THREAD_LOG_CHANNEL"])
-join_log_channel = int(os.environ["JOIN_LOG_CHANNEL"])
 log_channel = int(os.environ["LOG_CHANNEL"])
-vc_log_channel = int(os.environ["VC_LOG_CHANNEL"])
-error_log_channel = int(os.environ["ERROR_CHANNEL"])
-
-# Id-vc
-count_vc = int(os.environ["COUNT_VC"])
 
 
 # emoji
