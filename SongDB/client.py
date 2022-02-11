9b17f52ac723f1cf7ce7741aecc0f34a3d9a00d5
sushichaaaan <sushi_code@outlook.jp>
2022-02-12 07:27:21 +0900

    rename

--- SongDB/client.py
+++ SongDB/client.py
@@ -9,14 +9,14 @@ class SongDBClient(SongDBHttpClient):
     def __init__(self) -> None:
         super().__init__()
 
-    async def artist(self, artist_name: str) -> Artist:
-        return Artist(await self.search_by_artist(artist_name=artist_name))
+    async def search_artist(self, *, artist_name: str) -> Artist:
+        return Artist(await self._search_by_artist(artist_name=artist_name))
 
-    async def no_recent(self) -> No_Recent:
-        return No_Recent(await self.search_no_recent_song())
+    async def search_no_recent(self) -> No_Recent:
+        return No_Recent(await self._search_no_recent_song())
 
-    async def song(self, song_name: str) -> Song:
-        return Song(await self.search_by_song(song_name=song_name))
+    async def search_song(self, *, song_name: str) -> Song:
+        return Song(await self._search_by_song(song_name=song_name))
 
-    async def stream(self, stream_id: str) -> Stream:
-        return Stream(await self.search_by_stream(stream_id=stream_id))
+    async def search_stream(self, *, stream_id: str) -> Stream:
+        return Stream(await self._search_by_stream(stream_id=stream_id))
--- SongDB/http.py
+++ SongDB/http.py
@@ -8,19 +8,19 @@ class SongDBHttpClient:
     def __init__(self) -> None:
         pass
 
-    async def request(self, endpoint: str, **kwargs: Any) -> Any:
+    async def request(self, *, endpoint: str, **kwargs: Any) -> Any:
         url = self.BASE_URL + endpoint
         result: requests.Response = requests.post(url)
         return result
 
-    async def search_by_song(self, song_name: str) -> Any:
+    async def _search_by_song(self, *, song_name: str) -> Any:
         return await self.request(endpoint=f"?title={song_name}")
 
-    async def search_by_artist(self, artist_name: str) -> Any:
+    async def _search_by_artist(self, *, artist_name: str) -> Any:
         return await self.request(endpoint=f"?artist={artist_name}")
 
-    async def search_by_stream(self, stream_id: str) -> Any:
+    async def _search_by_stream(self, *, stream_id: str) -> Any:
         return await self.request(endpoint=f"?stream={stream_id}")
 
-    async def search_no_recent_song(self) -> Any:
+    async def _search_no_recent_song(self) -> Any:
         return await self.request(endpoint="?recent")
