5fa3c2a9ba45d6c07f56e4bf6c5c242271fffd3e
sushichaaaan <sushi_code@outlook.jp>
2022-02-12 07:40:23 +0900

    add docstring

--- SongDB/model/artist.py
+++ SongDB/model/artist.py
@@ -4,9 +4,19 @@ from SongDB.model.song import Song
 
 class Artist:
     def __init__(self, response: Any) -> None:
+        """A Object that has a list of the song related to a specific artist.
+
+        Args:
+            response (Any): A response from spreadsheet.
+        """
         self._response = response
         pass
 
     @property
     def songs(self) -> list[Song]:
+        """A list of the song related to a specific artist.
+
+        Returns:
+            list[Song]: A list contains each song's title and so on.
+        """
         return self._response
--- SongDB/model/history.py
+++ SongDB/model/history.py
@@ -2,14 +2,29 @@ from typing import Any
 
 
 class History:
+    """[summary]
+    An object that represents a history of the song.
+    """
+
     def __init__(self, response: Any) -> None:
         self._response = response
         pass
 
     @property
     def date(self) -> str:
+        """
+        A date that the song is sang.
+        Returns:
+            str: [description]
+        """
         return self._response
 
     @property
     def href(self) -> str:
+        """[summary]
+        A link that the song had begun singing.
+
+        Returns:
+            str: [description]youtube link like youtu.be/xxxx
+        """
         return self._response
