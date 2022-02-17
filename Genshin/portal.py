304849dd1757acfcc08bb1716446996ce32b33dd
sushichaaaan <sushi_code@outlook.jp>
2022-02-18 02:43:56 +0900

    gtool command

--- Genshin/portal.py
+++ Genshin/portal.py
@@ -1,4 +1,6 @@
 import discord
+from discord import ApplicationContext
+from discord.commands import slash_command
 from discord.ext import commands
 
 from . import cfg, embed_builder
@@ -8,10 +10,10 @@ class Portal(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="tool")
-    async def portal(self, ctx):
+    @slash_command(name="gtool")
+    async def portal(self, ctx: ApplicationContext):
         embed = embed_builder._portal()
-        await ctx.message.channel.send(embed=embed, view=PortalView())
+        await ctx.respond(embed=embed, view=PortalView(), ephemeral=True)
         return
 
 
@@ -19,11 +21,9 @@ class PortalView(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
         self.add_item(discord.ui.Button(label="公式マップ", url=cfg.official_map, row=0))
-        self.add_item(
-            discord.ui.Button(label="公式戦績", url=cfg.official_statistics, row=0)
-        )
-        self.add_item(discord.ui.Button(label="非公式マップ", url=cfg.unofficial_map, row=1))
+        self.add_item(discord.ui.Button(label="非公式マップ", url=cfg.unofficial_map, row=0))
         self.add_item(discord.ui.Button(label="コード受け取り", url=cfg.redeem_code, row=1))
+        self.add_item(discord.ui.Button(label="ログインボーナス", url=cfg.login_bonus, row=1))
 
 
 def setup(bot):
