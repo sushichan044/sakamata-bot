fa9241825fb3a82500c3b116c0e45c6a47a1ccb8
sushichaaaan <sushi_code@outlook.jp>
2022-02-17 19:08:08 +0900

    fix

--- Cogs/inquiry.py
+++ Cogs/inquiry.py
@@ -65,11 +65,9 @@ class InquiryConfView(discord.ui.View):
             type=thread_type,
         )
         await target.add_user(interaction.user)
-        mods = interaction.guild.get_role(admin_role).members
-        if mods:
-            for mod in mods:
-                await target.add_user(mod)
-        em = eb._inquiry_contact(target)
+        role = interaction.guild.get_role(admin_role)
+        await target.send(role.mention)
+        em = EB()._inquiry_contact(target)
         await interaction.followup.send(embed=em, ephemeral=True)
         return
 
