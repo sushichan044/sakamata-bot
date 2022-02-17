f126db74926f544e0509e275cbc92814621760c2
sushichaaaan <sushi_code@outlook.jp>
2022-02-17 16:54:17 +0900

    test recent

--- SongDB/embed_builder.py
+++ SongDB/embed_builder.py
@@ -124,34 +124,84 @@ class EmbedBuilder:
         s_method = [converter_dict[k] + v for k, v in input.items() if v]
         return s_method
 
-    def _many(self, *, input: dict, songs: list[Song]) -> str:
-        s_texts = []
-        nl = "\n"
-        dnl = "\n\n"
-        for num in range(len(songs)):
-            song = songs[num]
-            delta = self.calc_delta(song.latest.date)
-            if not song.latest.url:
-                availables = [history for history in song.history if history.url]
-                if availables != []:
-                    _watch = (
-                        f"最新のデータの配信にはアーカイブが存在しません。{nl}二番目に新しいものを視聴({availables[0].url})"
+    def _empty_recent(self, *, _to: str) -> Embed:
+        embed = Embed(
+            title="検索結果",
+            description="検索結果は0件でした。",
+            color=2105893,
+        )
+        embed.add_field(
+            name="検索条件",
+            value=f"{_to} 以前のデータ",
+        ),
+        return embed
+
+    def _recent(self, *, _to: str, songs: list[Song]) -> list[Embed]:
+        embeds: list[Embed] = []
+        embed = Embed(
+            title="検索結果",
+            description=f"検索結果は{len(songs)}件でした。",
+            color=2105893,
+        )
+        embed.add_field(
+            name="検索条件",
+            value=f"{_to} 以前のデータ",
+        ),
+        embeds.append(embed)
+        if songs != []:
+            for song in songs:
+                delta = self.calc_delta(song.latest.date)
+                embed = Embed(
+                    title=song.title,
+                    color=2105893,
+                )
+                embed.add_field(
+                    name="アーティスト",
+                    value=song.artist,
+                    inline=False,
+                )
+                embed.add_field(
+                    name="最終歌唱日",
+                    value=f"{song.latest.date}({str(delta.days)}日経過)",
+                    inline=False,
+                )
+                if song.latest.url:
+                    embed.add_field(
+                        name="視聴",
+                        value=f"[クリックして視聴]({song.latest.url})",
+                        inline=False,
                     )
-                _watch = "アーカイブが存在しません。"
-            _watch = f"視聴({song.latest.url})"
-            _text = f"""
-            [{str(num+1)}]{nl}
-            曲名: {song.title}{nl}
-            アーティスト名: {song.artist}{nl}
-            最終歌唱日: {song.latest.date}({str(delta.days)}日経過){nl}
-            {_watch}{nl}
-            """
-            s_texts.append(_text)
-        text = f"""
-        【歌枠データベース照会結果】{nl}
-        検索結果が10件を超えたため簡易表示に切り替えます。{nl}{nl}
-        検索条件:{nl}
-        {nl.join(self._query(input=input))}{nl}{nl}
-        {dnl.join(s_texts)}"""
-        print(len(text))
-        return text
+                    embed.set_thumbnail(
+                        url=f"https://img.youtube.com/vi/{song.latest.raw_id}/maxresdefault.jpg"
+                    )
+                else:
+                    embed.add_field(
+                        name="視聴不可",
+                        value="No archive",
+                        inline=False,
+                    )
+                if song.latest.note:
+                    embed.add_field(
+                        name="備考",
+                        value=song.latest.note,
+                        inline=False,
+                    )
+                if not song.latest.url:
+                    availables = [history for history in song.history if history.url]
+                    if availables != []:
+                        embed.add_field(
+                            name="視聴(二番目に新しいもの)",
+                            value=f"[クリックして視聴]({availables[0].url})",
+                            inline=False,
+                        )
+                        embed.set_thumbnail(
+                            url=f"https://img.youtube.com/vi/{availables[0].raw_id}/maxresdefault.jpg"
+                        )
+                        if availables[0].note:
+                            embed.add_field(
+                                name="備考",
+                                value=availables[0].note,
+                                inline=False,
+                            )
+                embeds.append(embed)
+        return embeds
--- SongDB/main.py
+++ SongDB/main.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime, timedelta, timezone
 
 import discord
 from Core.error import InteractionError
@@ -8,17 +9,15 @@ from discord.ext import commands
 from SongDBCore import SongDBClient
 
 from SongDB.embed_builder import EmbedBuilder as EB
-from SongDB.match import match_url
 from SongDB.many_page import PagePage
+from SongDB.match import match_url
 
-
-req_url = "https://script.google.com/macros/s/AKfycbyYXAOWYDQRe1___cQyPTZkKGC-BZfbpF4ksEpXIvJpAHPH8CO-I0yu0fNqpNCvT7M/exec"
+req_url = "https://script.google.com/macros/s/AKfycbybEQO66Ui5AbgaPvisluBbWMqxayLM2iyPCNeipXUOvn__Jp4SQsm7X8Z4w3HQvxja/exec"
 
 guild_id = int(os.environ["GUILD_ID"])
 
-# search_option:
-# 1: 曲で検索
-# 2: 歌枠で検索
+utc = timezone.utc
+jst = timezone(timedelta(hours=9), "Asia/Tokyo")
 
 
 class SongDB(commands.Cog):
@@ -59,10 +58,63 @@ class ProdDropdown(discord.ui.Select):
             await interaction.response.send_modal(modal=ProdSearch())
             return
         elif self.values[0] == "no_recent":
-            await interaction.response.send_message(content="準備中です。", ephemeral=True)
+            embed = EB()._start()
+            view = ProdRecentDropdownView()
+            await interaction.response.send_message(
+                embed=embed, view=view, ephemeral=True
+            )
             return
         else:
             raise InteractionError(interaction=interaction, cls=self)
+
+
+class DateSelect(discord.ui.Select):
+    def __init__(self) -> None:
+        options = [
+            discord.SelectOption(
+                label="1ヶ月",
+                value="1",
+                description="1ヶ月歌われていない曲を検索します。",
+                default=False,
+            ),
+            discord.SelectOption(
+                label="3ヶ月",
+                value="3",
+                description="3ヶ月歌われていない曲を検索します。",
+                default=False,
+            ),
+            discord.SelectOption(
+                label="6ヶ月",
+                value="6",
+                description="6ヶ月歌われていない曲を検索します。",
+                default=False,
+            ),
+            discord.SelectOption(
+                label="1年",
+                value="12",
+                description="1年歌われていない曲を検索します。",
+                default=False,
+            ),
+        ]
+        super().__init__(
+            placeholder="検索する期間を選択してください。", min_values=1, max_values=1, options=options
+        )
+
+    async def callback(self, interaction: discord.Interaction):
+        mth = int(self.values[0])  # 1 or 3 or 6 or 12
+        now = datetime.now().astimezone(jst)
+        __to = (now - timedelta(days=30 * mth)).date()
+        _to = f"{__to.year}/{__to.month}/{__to.day}"
+        client = SongDBClient(url=req_url)
+        _date = await client.search_by_date(_to=_to)
+        if _date.songs == []:  # no result found
+            embed = EB()._empty_recent(_to=_to)
+            await interaction.response.send_message(embed=embed, ephemeral=True)
+            return
+        embeds = EB()._recent(_to=_to, songs=_date.songs)
+        # await interaction.response.send_message(embed=embeds[0])
+        await PagePage(embeds=embeds)._send(interaction)
+        return
 
 
 class ProdSearch(discord.ui.Modal):
@@ -132,6 +184,12 @@ class ProdDropdownView(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
         self.add_item(ProdDropdown())
+
+
+class ProdRecentDropdownView(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+        self.add_item(DateSelect())
 
 
 def setup(bot):
