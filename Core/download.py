2bedf5558da3d89c4d222fd85ee3c16b5ea22300
sushichaaaan <sushi_code@outlook.jp>
2022-02-21 01:00:39 +0900

    fix

--- Core/download.py
+++ Core/download.py
@@ -3,9 +3,10 @@ import requests
 
 def download(title: str, url: str):
     try:
-        r = requests.get(url)
+        r = requests.get(url, stream=True)
         # openの中で保存先のパス（ファイル名を指定）
         with open("/tmp/" + title, mode="wb") as f:
-            f.write(r.content)
+            for chunk in r:
+                f.write(chunk)
     except requests.exceptions.RequestException as err:
         print(err)
