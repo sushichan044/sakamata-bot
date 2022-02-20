54ef4a4c07619f527a922a8cf0a5ad8df0a4e042
sushichaaaan <sushi_code@outlook.jp>
2022-02-21 01:20:47 +0900

    fix

--- Core/dm.py
+++ Core/dm.py
@@ -3,9 +3,10 @@ import os
 import discord
 from discord.ext import commands
 
-from . import embed_builder as EB
-from .confirm import Confirm
-from .log_sender import LogSender as LS
+from Core.confirm import Confirm
+from Core.download import download
+from Core.embed_builder import EmbedBuilder as EB
+from Core.log_sender import LogSender as LS
 
 admin_role = int(os.environ["ADMIN_ROLE"])
 
@@ -19,28 +20,58 @@ class DM_Sys(commands.Cog):
     @commands.command(name="send-dm")
     @commands.has_role(admin_role)
     async def _send_dm(
-        self, ctx: commands.Context, user: discord.Member, *, arg: str
+        self, ctx: commands.Context, user: discord.Member, *, text: str
     ) -> None:
         """DM送信用"""
-        role = ctx.guild.get_role(admin_role)
+        permitted_role = ctx.guild.get_role(admin_role)
         confirm_msg = f"【DM送信確認】\n以下のDMを{user.mention}へ送信します。"
         exe_msg = f"{user.mention}にDMを送信しました。"
         non_exe_msg = f"{user.mention}へのDM送信をキャンセルしました。"
-        confirm_arg = f"\n{arg}\n------------------------"
-        result = await Confirm(self.bot).confirm(ctx, confirm_arg, role, confirm_msg)
+        confirm_arg = f"\n{text}\n------------------------"
+        if ctx.message.attachments:
+            files: list[discord.File] = [
+                await attachment.to_file() for attachment in ctx.message.attachments
+            ]
+            result = await Confirm(self.bot).confirm(
+                ctx, confirm_arg, permitted_role, confirm_msg, files
+            )
+        else:
+            files = []
+            result = await Confirm(self.bot).confirm(
+                ctx, confirm_arg, permitted_role, confirm_msg
+            )
         if result:
+            if files != []:
+                names = []
+                for attachment in ctx.message.attachments:
+                    names.append(attachment.filename)
+                    path = os.path.join(
+                        os.path.dirname(__file__), f"/tmp/{attachment.filename}"
+                    )
+                    await attachment.save(fp=path, use_cached=True)
+                # print("complete download")
+                sent_files = [
+                    discord.File(
+                        os.path.join(os.path.dirname(__file__), f"/tmp/{name}"),
+                        filename=name,
+                        spoiler=False,
+                    )
+                    for name in names
+                ]
+                sent_message = await user.send(content=text, files=sent_files)
+                for name in names:
+                    os.remove(os.path.join(os.path.dirname(__file__), f"/tmp/{name}"))
+            else:
+                sent_message = await user.send(content=text)
             msg = exe_msg
-            message = await user.send(arg)
-            desc_url = message.jump_url
-            await ctx.send("Sended!")
-            await LS(self.bot).send_exe_log(ctx, msg, desc_url)
-            return
+            desc_url = sent_message.jump_url
+            await ctx.send("DM was sended!")
         else:
             msg = non_exe_msg
             desc_url = ""
             await ctx.send("Cancelled!")
-            await LS(self.bot).send_exe_log(ctx, msg, desc_url)
-            return
+        await LS(self.bot).send_exe_log(ctx, msg, desc_url)
+        return
 
     def make_deal_dm(self, deal: str, add_dm: str) -> str:
         DM_content = f"""【あなたは{deal}されました】
@@ -66,7 +97,7 @@ class DM_Sys(commands.Cog):
                 channel = self.bot.get_channel(dm_box_channel)
                 embeds = []
                 if message.content or message.attachments:
-                    embed = EB.compose_embed_dm_box(message)
+                    embed = EB().compose_embed_dm_box(message)
                     embeds.append(embed)
                     for attachment in message.attachments[1:]:
                         embed = discord.Embed(
