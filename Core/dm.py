69a264f8890cb1237dd3184fa076421789ab8ff8
sushichaaaan <sushi_code@outlook.jp>
2022-02-20 23:21:03 +0900

    add support for attach image

--- Core/dm.py
+++ Core/dm.py
@@ -19,28 +19,40 @@ class DM_Sys(commands.Cog):
     @commands.command(name="send-dm")
     @commands.has_role(admin_role)
     async def _send_dm(
-        self, ctx: commands.Context, user: discord.Member, *, arg: str
+        self, ctx: commands.Context, user: discord.Member, *, text: str
     ) -> None:
         """DM送信用"""
-        role = ctx.guild.get_role(admin_role)
+        permitted_role = ctx.guild.get_role(admin_role)
         confirm_msg = f"【DM送信確認】\n以下のDMを{user.mention}へ送信します。"
         exe_msg = f"{user.mention}にDMを送信しました。"
         non_exe_msg = f"{user.mention}へのDM送信をキャンセルしました。"
-        confirm_arg = f"\n{arg}\n------------------------"
-        result = await Confirm(self.bot).confirm(ctx, confirm_arg, role, confirm_msg)
+        confirm_arg = f"\n{text}\n------------------------"
+        if ctx.message.attachments:
+            files: list[discord.File] = [
+                await attachment.to_file() for attachment in ctx.message.attachments
+            ]
+            result = await Confirm(self.bot).confirm(
+                ctx, confirm_arg, permitted_role, confirm_msg, files
+            )
+        else:
+            files = []
+            result = await Confirm(self.bot).confirm(
+                ctx, confirm_arg, permitted_role, confirm_msg
+            )
         if result:
+            if files != []:
+                sent_message = await user.send(content=text, files=files)
+            else:
+                sent_message = await user.send(content=text)
             msg = exe_msg
-            message = await user.send(arg)
-            desc_url = message.jump_url
-            await ctx.send("Sended!")
-            await LS(self.bot).send_exe_log(ctx, msg, desc_url)
-            return
+            desc_url = sent_message.jump_url
+            await ctx.send("DM was sended!")
         else:
             msg = non_exe_msg
             desc_url = ""
             await ctx.send("Cancelled!")
-            await LS(self.bot).send_exe_log(ctx, msg, desc_url)
-            return
+        await LS(self.bot).send_exe_log(ctx, msg, desc_url)
+        return
 
     def make_deal_dm(self, deal: str, add_dm: str) -> str:
         DM_content = f"""【あなたは{deal}されました】
