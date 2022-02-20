6ab4aa6be224ac4dcb1d73a0b5dca741d119f387
sushichaaaan <sushi_code@outlook.jp>
2022-02-21 00:40:13 +0900

    fix image handle

--- Core/message.py
+++ Core/message.py
@@ -5,6 +5,7 @@ from discord.ext import commands
 from newdispanderfixed import dispand
 
 from Core.confirm import Confirm
+from Core.download import download
 from Core.log_sender import LogSender as LS
 
 admin_role = int(os.environ["ADMIN_ROLE"])
@@ -16,15 +17,23 @@ class Message_Sys(commands.Cog):
 
     @commands.Cog.listener("on_message")
     async def on_message_dispand(self, message: discord.Message):
-        avoid_word_list_prefix = ["//send-message", "//edit-message", "//send-dm"]
+        avoid_prefix_list = [
+            "//send-message",
+            "//edit-message",
+            "//send-dm",
+            "【メッセージ送信確認】",
+            "【メッセージ編集確認】",
+        ]
+        avoid_suffix_list = []
         if type(message.channel) == discord.DMChannel:
             return
         else:
-            for word in avoid_word_list_prefix:
-                if message.content.startswith(word):
+            for prefix in avoid_prefix_list:
+                if message.content.startswith(prefix):
                     return
-            if message.content.endswith("中止に必要な承認人数: 1"):
-                return
+            for suffix in avoid_suffix_list:
+                if message.content.endswith(suffix):
+                    return
             else:
                 await dispand(self.bot, message)
                 return
@@ -55,7 +64,25 @@ class Message_Sys(commands.Cog):
             )
         if result:
             if files != []:
-                sent_message = await channel.send(content=text, files=files)
+                names = []
+                for attachment in ctx.message.attachments:
+                    names.append(attachment.filename)
+                    if attachment.proxy_url:
+                        download(attachment.filename, attachment.proxy_url)
+                    else:
+                        download(attachment.filename, attachment.url)
+                # print("complete download")
+                sent_files = [
+                    discord.File(
+                        os.path.join(os.path.dirname(__file__), f"/tmp/{name}"),
+                        filename=name,
+                        spoiler=False,
+                    )
+                    for name in names
+                ]
+                sent_message = await channel.send(content=text, files=sent_files)
+                for name in names:
+                    os.remove(os.path.join(os.path.dirname(__file__), f"/tmp/{name}"))
             else:
                 sent_message = await channel.send(content=text)
             msg = exe_msg
@@ -87,15 +114,42 @@ class Message_Sys(commands.Cog):
         non_exe_msg = f"{channel.mention}のメッセージの編集をキャンセルしました。"
         confirm_arg = f"\n{text}\n------------------------"
         if ctx.message.attachments:
-            await ctx.reply(content="メッセージの編集でファイルを追加することはできません。")
-            return
-        result = await Confirm(self.bot).confirm(
-            ctx, confirm_arg, permitted_role, confirm_msg
-        )
-        if result:
-            sent_message = await target.edit(
-                content=text, attachments=target.attachments
+            files: list[discord.File] = [
+                await attachment.to_file() for attachment in ctx.message.attachments
+            ]
+
+            result = await Confirm(self.bot).confirm(
+                ctx, confirm_arg, permitted_role, confirm_msg, files
             )
+        else:
+            files = []
+            result = await Confirm(self.bot).confirm(
+                ctx, confirm_arg, permitted_role, confirm_msg
+            )
+        if result:
+            if files != []:
+                names = []
+                for attachment in ctx.message.attachments:
+                    names.append(attachment.filename)
+                    if attachment.proxy_url:
+                        download(attachment.filename, attachment.proxy_url)
+                    else:
+                        download(attachment.filename, attachment.url)
+                # print("complete download")
+                sent_files = [
+                    discord.File(
+                        os.path.join(os.path.dirname(__file__), f"/tmp/{name}"),
+                        filename=name,
+                        spoiler=False,
+                    )
+                    for name in names
+                ]
+                sent_message = await target.edit(content=text, files=sent_files)
+                for name in names:
+                    os.remove(os.path.join(os.path.dirname(__file__), f"/tmp/{name}"))
+                # print("complete delete")
+            else:
+                sent_message = await target.edit(content=text)
             msg = exe_msg
             desc_url = sent_message.jump_url
             await ctx.send("Edited!")
