a622e0d9c7e625e9cfb8dba6bf071e710ddc0bd5
sushichaaaan <sushi_code@outlook.jp>
2022-02-21 01:28:57 +0900

    fix

--- Core/message.py
+++ Core/message.py
@@ -70,7 +70,7 @@ class Message_Sys(commands.Cog):
                     path = os.path.join(
                         os.path.dirname(__file__), f"/tmp/{attachment.filename}"
                     )
-                    await attachment.save(fp=path, use_cached=True)
+                    await attachment.save(fp=path)
                 print(names)
                 # print("complete download")
                 sent_files = [
@@ -135,7 +135,7 @@ class Message_Sys(commands.Cog):
                     path = os.path.join(
                         os.path.dirname(__file__), f"/tmp/{attachment.filename}"
                     )
-                    await attachment.save(fp=path, use_cached=True)
+                    await attachment.save(fp=path)
                 # print("complete download")
                 sent_files = [
                     discord.File(
