196eca43aeee400519ce5c65ebf28b64c5b64688
sushichaaaan <sushi_code@outlook.jp>
2022-02-23 00:42:21 +0900

    initial commit

---
+++ security/anti_spam.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+import os
+
+import discord
+from discord import Message
+from discord.ext import commands
+
+admin_role = int(os.environ["ADMIN_ROLE"])
+
+
+class AntiSpam(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.Cog.listener("on_message")
+    async def _anti_rapid_post(self, message: Message):
+        if message.channel.type == discord.DMChannel:
+            return
+        if admin_role in [r.id for r in message.author.roles]:
+            return
+
+        def check_rapid_post(m: Message):
+            return (
+                m.author == message.author
+                and (datetime.utcnow() - m.created_at).seconds < 15
+            )
+
+        if (
+            len(list(filter(lambda m: check_rapid_post(m), self.bot.cached_messages)))
+            >= 5
+        ):
+            print("連投を検知")
+            return  # alert 連投
+
+
+def setup(bot):
+    return bot.add_cog(AntiSpam(bot))
